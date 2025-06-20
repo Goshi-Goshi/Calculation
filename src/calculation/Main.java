fe1d2691903b5941b9d8c1d09bf767c53c3dce51
Goshi-Goshi <goshima@mark-bear.com>
2025-06-20 21:25:49 +0900

    自宅PCでコミット（プッシュせず）

--- src/calculation/Main.java
+++ src/calculation/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("自宅PCでCalcプロジェクトを実行しています。");
-
+		System.out.println("このテキストは自宅PCで入力し、プッシュしていません。");
 
 	}
 
