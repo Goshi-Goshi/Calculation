0eaae3efc12b7cdbc4c951d587885ce7e39130a3
Goshi-Goshi <goshima@mark-bear.com>
2025-06-20 21:42:37 +0900

    コメントアウトをプッシュ

--- src/calculation/Main.java
+++ src/calculation/Main.java
@@ -5,8 +5,26 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("自宅PCでCalcプロジェクトを実行しています。");
-		System.out.println("このテキストは自宅PCで入力し、プッシュしていません。");
 
+
+		/*
+		▽ VS Codeのインストールについて
+https://www.insource-da.co.jp/dxpedia/02_0001.html
+
+▽ インストール時に出てくるウィザードの「追加タスク」について
+https://creating-homepage.com/archives/8478
+
+▽ 拡張機能でjavaとスプリングブートをインストール
+・Extension pack for Java
+・Spring boot Extension pack(不要な場合はなし)
+Extension parkは拡張セット付という意味。
+
+▽ javaが入ってなければJDKをインストール
+
+
+▽ Gitをインストール（Gitはローカルで動いて、GitHubと連携してくれます）
+https://zenn.dev/kd_gamegikenblg/articles/b220e23b0b7ef9
+*/
 	}
 
 }
